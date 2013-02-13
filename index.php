ac5ea8e336e522a20e8e55ff64c49705f6e19c30
Tomasz Muras <nexor1984@gmail.com>
2013-02-13 12:53:03 +0100

    A bit of a clean-up.

--- index.php
+++ index.php
@@ -8,12 +8,6 @@ require_once($CFG->dirroot . '/lib/statslib.php');
 require_once($CFG->libdir . '/adminlib.php');
 require_once($CFG->dirroot . '/enrol/locallib.php');
 
-//define variables
-//$message = "";
-//$table_start = "<table><tr><th rowspan='2'>Course</th><th colspan='4'>Enrolled Users</th></tr>
-//                <tr><th>Teachers</th><th>Students</th><th>Other</th><th>Total</th>";
-//$table_end = "</table>";
-
 $report_table = new html_table();
 $report_table->head = array("Course", "Enrolled Teachers", "Enrolled Students", "Enrolled Other Users", "Total");
 $report_table->data = array();
@@ -36,90 +30,6 @@ $navigation->add(get_string('pluginname', 'report_simplereport'), $url, navigati
         new pix_icon('i/report', ''));
 
 
-//select all courses and count students on each course
-/* $result = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =5 group by c.id');
- */
-
-/*
-  $result = $DB->get_records_sql('SELECT c.id, ra.id, ra.roleid, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  group by c.id');
-  //print_object($result);
-  $teachers = 0;
-  $students = 0;
-  $other = 0;
-  //loop throught each record
-  foreach($result as $record){
-  //print_object($record);
-  $course_name = $record->fullname;
-  $totals = $record->total;
-
-  print $record->roleid;
-  if($record->roleid==5){
-  $students++;
-  }else if($record->roleid==3){
-  $teachers++;
-  }else{
-  $other++;
-  }
-
-
-
-
-  $message = $message . "<tr><td>$course_name</td><td>$teachers</td><td>$students</td><td>$other</td><td>$totals</td></tr>";
-
-  }
-
- */
-/*
-  $result_students = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =5 group by c.id');
-  //loop throught each record
-  foreach($result_students as $record){
-  $course_name = $record->fullname;
-  $students = $record->total;
- */
-
-/*
-  $result_students = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =5
-  group by c.id');
-  $result_teachers = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =3
-  group by c.id');
-  $result_all = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  group by c.id');
- */
 
 //get total number of courses
 $totalcourses = $DB->count_records_sql('SELECT count(id) FROM {course} WHERE shortname <> ?', array('mysite'));
@@ -166,59 +76,6 @@ AND cxt.contextlevel =50
 AND cxt.instanceid = c.id 
 group by c.id ' . $limit);
 
-/*
-  if ($pagingbar->page == 0) {
-  $result_students = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =5
-  group by c.id LIMIT 0, 5');
-  $result_teachers = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =3
-  group by c.id LIMIT 0, 5');
-  $result_all = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  group by c.id LIMIT 0, 5');
-  } else {
-  $result_students = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =5
-  group by c.id LIMIT 5, 2');
-  $result_teachers = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  AND ra.roleid =3
-  group by c.id LIMIT 5, 2');
-  $result_all = $DB->get_records_sql('SELECT ra.id, c.id, c.fullname, count(u.username) as total
-  FROM {role_assignments} ra, {user} u, {course} c, {context} cxt
-  WHERE ra.userid = u.id
-  AND ra.contextid = cxt.id
-  AND cxt.contextlevel =50
-  AND cxt.instanceid = c.id
-  group by c.id LIMIT 5, 2');
-  }
- */
-
-
 
 //loop throught each record
 foreach ($result_students as $record) {
@@ -240,25 +97,10 @@ foreach ($courses as $course) {
     $i++;
 }
 
-//$total = count($courses);
-
-
-/*
-  $table_data = $report_table->data;
-  $page1_data = array_slice($table_data, 0, 5);
-  $page2_data = array_slice($table_data, 6, (count($table_data) - 1));
- */
 //output report on the page
 echo $OUTPUT->header();
 //echo $table_start . $message . $table_end;
 
-/*
-  if ($pagingbar->page == 0) {
-  $report_table->data = $page1_data;
-  } else {
-  $report_table->data = $page2_data;
-  }
- */
 echo html_writer::table($report_table);
 echo $OUTPUT->render($pagingbar);
 echo $OUTPUT->footer();
